40dc823fe021e00e97a2b21784ca238bfb0f6eb4
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-07-08 14:24:13 +0700

    add config module and database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,21 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { MovieModule } from './movie/movie.module';
 import { ReviewModule } from './review/review.module';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './config/env.validate';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, UserModule, MovieModule, ReviewModule]
+  imports: [
+    AuthModule,
+    UserModule,
+    MovieModule,
+    ReviewModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate
+    }),
+    DatabaseModule
+  ]
 })
 export class AppModule {}
---
+++ src/config/env.validate.ts
@@ -0,0 +1,24 @@
+import { Logger } from '@nestjs/common';
+import z from 'zod';
+
+const envSchema = z.object({
+  PORT: z.coerce.number().int().positive().max(65535),
+  DATABASE_URL: z.url()
+});
+
+export function validate(config: Record<string, any>) {
+  const parsed = envSchema.safeParse(config);
+
+  if (!parsed.success) {
+    const logger = new Logger('EnvValidation');
+    logger.error(
+      'Env variables validation failed',
+      z.prettifyError(parsed.error)
+    );
+    throw new Error('Env variables validation failed');
+  }
+
+  return parsed.data;
+}
+
+export type EnvVariable = z.infer<typeof envSchema>;
